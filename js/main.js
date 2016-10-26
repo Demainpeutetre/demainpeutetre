9b45470e3995c70e7e5b040b2bd5ba367dec5beb
Sylvain Martin <sylvainmdev@gmail.com>
2016-10-26 20:47:21 +0200

    pagination

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 var loadButton = $('#pagination');
 
 var feed = new Instafeed({
-<<<<<<< HEAD
 	get: 'user',
 	userId: 4034986418,
 	clientId: 'c82f268229d34673b0b4c1d6a75222b0',
@@ -16,31 +15,12 @@ var feed = new Instafeed({
 		if (!this.hasNext())
 			loadButton.addClass('hidden');
 	},
-=======
-		get: 'user',
-		userId: 4034986418,
-		clientId: 'c82f268229d34673b0b4c1d6a75222b0',
-		accessToken: '4034986418.1677ed0.a4168a68457c4a62a6ebae8d77340ace',
-		target: 'instagram',
-		limit: 60,
-		resolution: 'standard_resolution',
-		template: '<a class="fancybox" rel="gallery1" href="{{model.images.standard_resolution.url}}"><img class="instagram-image" src="{{image}}" /></a>',
-		after: function() {
-			var el = document.getElementById('instagram');
-			if (el.classList)
-					el.classList.add('show');
-			else
-					el.className += ' ' + 'show';
-		}
->>>>>>> 62a93ce70c8f2a4c40d89659f425224d4d4969a5
 });
-
 
 // bind the load more button
 loadButton.addEventListener('click', function() {
 	feed.next();
 });
-
 
 feed.run();
 
