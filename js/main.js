0b7173eb61f4f9d0da644e05429472d77b508f9d
Sylvain Martin <sylvainmdev@gmail.com>
2016-10-26 20:44:58 +0200

    pagination

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 //INSTAFEED
 
+var loadButton = $('#pagination');
+
 var feed = new Instafeed({
-		get: 'user',
-		userId: 4034986418,
-		clientId: 'c82f268229d34673b0b4c1d6a75222b0',
-		accessToken: '4034986418.1677ed0.a4168a68457c4a62a6ebae8d77340ace',
-		target: 'instagram',
-		resolution: 'standard_resolution',
-		template: '<a class="fancybox" rel="gallery1" href="{{model.images.standard_resolution.url}}"><img class="instagram-image" src="{{image}}" /></a>',
-		after: function() {
-			var el = document.getElementById('instagram');
-			if (el.classList)
-					el.classList.add('show');
-			else
-					el.className += ' ' + 'show';
-		}
+	get: 'user',
+	userId: 4034986418,
+	clientId: 'c82f268229d34673b0b4c1d6a75222b0',
+	accessToken: '4034986418.1677ed0.a4168a68457c4a62a6ebae8d77340ace',
+	target: 'instagram',
+	resolution: 'standard_resolution',
+	template: '<a class="fancybox" rel="gallery1" href="{{model.images.standard_resolution.url}}"><img class="instagram-image" src="{{image}}" /></a>',
+	after: function() {
+		// disable button if no more results to load
+		if (!this.hasNext())
+			loadButton.addClass('hidden');
+	},
 });
+
+
+// bind the load more button
+loadButton.addEventListener('click', function() {
+	feed.next();
+});
+
+
 feed.run();
 
 //NAVBAR
@@ -28,7 +36,7 @@ $(document).ready(function(){
 	// fade in .navbar
 	$(function () {
 		$(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
+			// set distance user needs to scroll before we fadeIn navbar
 			if ($(this).scrollTop() > 115) {
 				$('.navbar').fadeIn();
 			} else {
@@ -48,18 +56,18 @@ $('#instagram').addClass('animated fadeIn');
 //FANCYBOX
 
 $(document).ready(function() {
-		$(".fancybox").fancybox({
-			openEffect	: 'elastic',
-    	closeEffect	: 'elastic',
-		});
+	$(".fancybox").fancybox({
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic',
 	});
+});
 
 //PROFILE
 
 (function($) {
-  $(function() {
-    $('.toggle-overlay').click(function() {
-      $('aside').toggleClass('open');
-    });
-  });
+	$(function() {
+		$('.toggle-overlay').click(function() {
+			$('aside').toggleClass('open');
+		});
+	});
 })(jQuery);
