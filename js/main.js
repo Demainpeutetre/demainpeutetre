91859a2a29c809d91a8a9887fdf0538890c28eea
Sylvain Martin <sylvainmdev@gmail.com>
2016-10-26 23:06:33 +0200

    debug

--- js/main.js
+++ js/main.js
@@ -1,64 +1,97 @@
-//INSTAFEED
+//update tags list from instafeed
+function updateTags() {
+    var html = '';
+    for (var e in tags) {
+        html = html + '<li>' + tags[e] + '</li>';
+     }
+    $('#tags').html(html);
+}
 
+//click on tag
+$('#tags li').on("click", function() {
+	alert('toto');
+    var c = $(this).html();
+    $('#instagram img').each(function () {
+        if (!$(this).hasClass(c)) {
+            $(this).hide();
+        }
+        else {
+            $(this).show();
+        }
+    });
+});
+
+//result buffer
 var result = null;
 
+//init tags list
+var tags = [];
+
+//init instafeed
 var feed = new Instafeed({
-	get: 'user',
-	userId: 4034986418,
-	clientId: 'c82f268229d34673b0b4c1d6a75222b0',
-	accessToken: '4034986418.1677ed0.a4168a68457c4a62a6ebae8d77340ace',
-	target: 'instagram',
-	resolution: 'standard_resolution',
-	template: '<a class="fancybox" rel="gallery1" href="{{model.images.standard_resolution.url}}"><img class="instagram-image" src="{{image}}" /></a>',
-	after: function() {
+    get: 'user',
+    userId: 4034986418,
+    clientId: 'c82f268229d34673b0b4c1d6a75222b0',
+    accessToken: '4034986418.1677ed0.a4168a68457c4a62a6ebae8d77340ace',
+    target: 'instagram',
+    resolution: 'standard_resolution',
+    template: '<a class="fancybox" rel="gallery1" href="{{model.images.standard_resolution.url}}"><img class="instagram-image" src="{{image}}" /></a>',
+    after: function () {
 
-		for(var i in result.data) {
+        //set image classes with first tag
+        for (var i in result.data) {
+            if (result.data[i].tags.length) {
+                $('#instagram img').each(function (index) {
+                    if (index == i) {
+                        var tag = result.data[i].tags[0].substr(0);
+                        tags.push(tag);
+                        $(this).addClass(tag);
+                    }
+                });
+            }
+        }
 
-			if (result.data[i].tags.length) {
-				$('#instagram img').each(function(index) {
-					if (index == i) {
-						$(this).addClass(result.data[i].tags[0].substr(0));
-					}
-				});
-			}
+        //update tags
+        updateTags();
 
-		}
-		// disable button if no more results to load
-		if (!this.hasNext())
-		$('#pagination').addClass('hidden');
-	},
-	success: function(r) {
-		result = r;
-	}
+        // disable button if no more results to load
+        if (!this.hasNext()) {
+            $('#pagination').addClass('hidden');
+        }
+
+    },
+    success: function (r) {
+        result = r;
+    }
 });
 
 // bind the load more button
-$('#pagination').click(function() {
-	feed.next();
+$('#pagination').click(function () {
+    feed.next();
 });
 
 feed.run();
 
 //NAVBAR
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-	// hide .navbar first
-	$(".navbar").hide();
+    // hide .navbar first
+    $(".navbar").hide();
 
-	// fade in .navbar
-	$(function () {
-		$(window).scroll(function () {
-			// set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop() > 115) {
-				$('.navbar').fadeIn();
-			} else {
-				$('.navbar').fadeOut();
-			}
-		});
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+            if ($(this).scrollTop() > 115) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
 
 
-	});
+    });
 
 });
 
@@ -68,19 +101,19 @@ $('#instagram').addClass('animated fadeIn');
 
 //FANCYBOX
 
-$(document).ready(function() {
-	$(".fancybox").fancybox({
-		openEffect	: 'elastic',
-		closeEffect	: 'elastic',
-	});
+$(document).ready(function () {
+    $(".fancybox").fancybox({
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+    });
 });
 
 //PROFILE
 
-(function($) {
-	$(function() {
-		$('.toggle-overlay').click(function() {
-			$('aside').toggleClass('open');
-		});
-	});
+(function ($) {
+    $(function () {
+        $('.toggle-overlay').click(function () {
+            $('aside').toggleClass('open');
+        });
+    });
 })(jQuery);
