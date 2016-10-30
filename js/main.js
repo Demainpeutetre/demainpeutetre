e076e52d0398a4834fb969871f8b5a873294f12a
Sylvain Martin <sylvainmdev@gmail.com>
2016-10-30 17:59:33 +0100

    Change font, tags navigation and logo

--- js/main.js
+++ js/main.js
@@ -145,3 +145,18 @@ $(document).ready(function () {
         });
     });
 })(jQuery);
+
+/*
+  Slidemenu
+*/
+(function() {
+  var $body = document.body,
+    $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+
+  if (typeof $menu_trigger !== 'undefined') {
+    $menu_trigger.addEventListener('click', function() {
+      $body.className = ($body.className == 'menu-active') ? '' : 'menu-active';
+    });
+  }
+
+}).call(this);
